d09546e838994ba951dcdd3dcee43bfa5345ae11
Ahmed Abdel basset <107061768+Ahmedbm98@users.noreply.github.com>
2025-07-22 03:32:27 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -121,11 +121,13 @@ function Login() {
           <img
             src={meetusvr}
             alt="meetusvr"
+            loading={lazy}
             className=" w-full max-h-[700px] object-contain"
           />
           <img
             src={meetuslogo}
             alt="meetusvr logo"
+            loading={lazy}
             className="absolute left-[50%] translate-x-[-50%] top-[70%]"
           />
         </div>
